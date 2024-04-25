a9349174825444935a0bc09a63a194e0164aa3ea
Ariel Nogueira <arielnnogueira@hotmail.com>
2024-04-25 19:21:05 -0300

    [feature] create file router

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from "@/components";
+import {Home} from "@/pages/home";
+import {List} from "@/pages/list";
+
+export function Router (){
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/save" element={<Home />} />
+        <Route path="/list" element={<List />} />
+      </Route>
+    </Routes>
+  )
+}
